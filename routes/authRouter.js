c91aef3f2ad75a8ec975da78a62e6db7a6969ed5
jannick <jannickpedersen@live.dk>
2024-02-22 20:56:59 +0100

    setup controllers and routes for regi and login with validation and hash

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 const router = Router();
 import { register, login } from '../controllers/authController.js';
+import { validateRegisterInput, validateLoginInput } from '../middleware/validationMiddleware.js';
 
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', validateRegisterInput,  register);
+router.post('/login', validateLoginInput, login);
 
 export default router;
