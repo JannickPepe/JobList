773177f48a081dc6b23b766a8fcd64ab71a38add
jannick <jannickpedersen@live.dk>
2024-03-01 17:30:06 +0100

    setup the admin page with get users and jobs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import { loader as allJobsLoader } from './pages/AllJobs';
 import { loader as editJobLoader } from './pages/EditJob';
 import { action as editJobAction } from './pages/EditJob';
 import { action as deleteJobAction } from './pages/DeleteJob';
+import { loader as adminLoader } from './pages/Admin';
 
 
 //
@@ -69,6 +70,7 @@ const router = createBrowserRouter([
           {
             path: 'admin',
             element: <Admin />,
+            loader: adminLoader,
           },
           {
             path: 'edit-job/:id',
--- client/src/components/NavLinks.jsx
+++ client/src/components/NavLinks.jsx
@@ -6,12 +6,15 @@ import { NavLink } from 'react-router-dom';
 
 // eslint-disable-next-line react/prop-types
 const NavLinks = ({ isBigSidebar }) => {
+
   const { toggleSidebar, user } = useDashboardContext();
+
   return (
     <div className='nav-links'>
       {links.map((link) => {
         const { text, path, icon } = link;
         const { role } = user;
+        
         // Check if you are admin or not - for showing admin menu
         if (path === 'admin' && role !== 'admin') return;
         return (
@@ -22,12 +25,16 @@ const NavLinks = ({ isBigSidebar }) => {
             onClick={isBigSidebar ? null : toggleSidebar}
             end
           >
-            <span className='icon'>{icon}</span>
+            <span className='icon'>
+              {icon}
+            </span>
             {text}
           </NavLink>
         );
       })}
     </div>
   );
+
 };
+
 export default NavLinks;
